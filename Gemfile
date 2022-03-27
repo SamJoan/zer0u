131dae7add23c0efff142339c77819416e0f3df4
Sam Joan <sam@roque-worcel.com>
2022-03-28 11:37:11 +1300

    Add user authentication.

--- Gemfile
+++ Gemfile
@@ -72,3 +72,4 @@ group :test do
 end
 
 gem "pg", "~> 1.3"
+gem 'devise'
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,6 @@
 class DocumentsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @documents = Document.all
   end
